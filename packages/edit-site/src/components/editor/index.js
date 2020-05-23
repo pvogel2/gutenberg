1c21b7ac802e8a6ed74e13bc9353bdf54df1bd6a
epiqueras <epiquerass@gmail.com>
2020-05-23 16:14:47 -0700

    Edit Site: New template creation flow.

--- packages/edit-site/src/components/editor/index.js
+++ packages/edit-site/src/components/editor/index.js
@@ -94,14 +94,15 @@ function Editor( { settings: _settings } ) {
 		[]
 	);
 
-	const blockContext = useMemo( () => {
-		if ( ! settings.page.context.queryContext )
-			return settings.page.context;
-
-		return {
+	// Set default query for misplaced Query Loop blocks, and
+	// provide the root `queryContext` for top-level Query Loop
+	// and Query Pagination blocks.
+	const blockContext = useMemo(
+		() => ( {
 			...settings.page.context,
+			query: settings.page.context.query || { categoryIds: [] },
 			queryContext: [
-				settings.page.context.queryContext,
+				settings.page.context.queryContext || { page: 1 },
 				( newQueryContext ) =>
 					setSettings( ( prevSettings ) => ( {
 						...prevSettings,
@@ -117,8 +118,9 @@ function Editor( { settings: _settings } ) {
 						},
 					} ) ),
 			],
-		};
-	}, [ settings.page.context ] );
+		} ),
+		[ settings.page.context ]
+	);
 	return template ? (
 		<>
 			<EditorStyles styles={ settings.styles } />
